9b4d207b8a6dcba9a5d73aa7732c7308075601ac
andrew <uandrew1965@gmail.com>
2017-07-17 11:28:04 +0300

    Remove test subfolder

--- test/src/main/scala/ru/mfms/kamon/SysData.scala
+++
@@ -1,3 +0,0 @@
-package ru.mfms.kamon
-
-case class SysData(hostname: String = "test_host", containerId: String = "test_container")
--- test/src/main/scala/ru/mfms/kamon/package.scala
+++
@@ -1,7 +0,0 @@
-package ru.mfms
-
-package object kamon {
-    implicit class StrOpt(string: String) {
-        def normalize = string.replace(".", "_").replace(":", "_")
-    }
-}
